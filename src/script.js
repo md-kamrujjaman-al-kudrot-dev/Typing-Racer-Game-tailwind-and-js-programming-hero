dd2b39c9ef1ccee9a6c9153caf24727f2cdbdf42
md-kudrot-592 <kk.mdkudrot@gmail.com>
2025-05-24 21:34:51 +0600

    car one and  constant move, randomword funtionalaty done

--- src/script.js
+++ src/script.js
@@ -1,13 +1,15 @@
 const carOne = document.getElementById("car1")
 const carTwo = document.getElementById("car2")
+const wordBox = document.getElementById("wordBox")
+const inputWord = document.getElementById("typer-word")
+const modal = document.getElementById("modal")
+
 
 console.log("hello")
-setTimeout(()=>{
+setTimeout(() => {
     carOne.style.left = '160%'
     carTwo.style.left = '160%'
-},100)
-
-
+}, 100)
 
 
 
@@ -15,24 +17,97 @@ setTimeout(()=>{
 let carOnePOsition = 150
 let carTwoPOsition = 150
 let isGameOver = false
-const carOneConstantMove = ()=>{
+const words = [
+    "kamrujjaman",
+    "habib",
+    "rayhan",
+    "maruf",
+    "sajjad",
+    "rofik",
+    "the",
+    "quick",
+    "brown",
+    "fox",
+    "jumps",
+    "over",
+    "lazy",
+    "dog",
+    "sun",
+    "moon",
+    "start",
+    "tree",
+    "river",
+    "hill",
+    "skill"
+]
+
+const typeHandler = () => {
+    const randomWord = words[Math.floor(Math.random() * words.length)]
+    wordBox.innerText = randomWord
+
+    inputWord.addEventListener("input", (e) => {
+        if (e.target.value === randomWord) {
+            inputWord.value = "";
+            carTwoPOsition += 15;
+            carTwo.style.left = carTwoPOsition + "%";
+            if (carTwoPOsition >= 490) {
+                isGameOver = true;
+                winner("You win")
+            }
+            typeHandler()
+        }
+    })
+}
+
+const winner = (stats) => {
+    modal.innerHTML = `
+        <div class="bg-green-600 p-14   h-52  flex flex-col items-center justify-center">
+      <h2 class="text-5xl font-bold"> ${stats} </h2>
+      <h2 class="text-3xl">Your WPM: 30</h2>
+    </div>
+    `
+}
+
+
+const carOneConstantMove = () => {
     const carOneMOve = setInterval(() => {
-        if (isGameOver){
+        if (isGameOver) {
             clearInterval(carOneMOve)
         }
-        if(carOnePOsition >= 470){
+        if (carOnePOsition >= 490) {
             isGameOver = true
+            winner("You Lost")
         }
-        carOnePOsition += 3
+        carOnePOsition += 0.7
         carOne.style.left = carOnePOsition + "%"
+    }, 30);
+}
+
+const carTwoConstantMove = () => {
+    const carTwoMove = setInterval(() => {
+        if (isGameOver) {
+            clearInterval(carTwoMove)
+        }
+
+        if (carTwoPOsition >= 490) {
+            isGameOver = true
+            winner("You Win")
+
+        }
+        carTwoPOsition += 0.5 
+        carTwo.style.left = carTwoPOsition + "%"
     }, 30);
 }
 
 
 
 
+const startGame = () => {
+    setTimeout(() => {
+        carOneConstantMove()
+        carTwoConstantMove()
+    }, 1000);
 
-const startGame = ()=>{
-    carOneConstantMove()
+    typeHandler()
 }
 startGame()
